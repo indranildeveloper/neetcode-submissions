82fdac15892bce85bcad5c944823f770391ab9a6
Indranil Halder <73965619+indranildeveloper@users.noreply.github.com>
2026-08-17 21:30:37 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # Time Complexity -> O(n)
+        # Space Complexity -> O(n)
+        word1Pointer = 0
+        word2Pointer = 0
+
+        result = []
+
+        while word1Pointer < len(word1) and word2Pointer < len(word2):
+            result.append(word1[word1Pointer])
+            result.append(word2[word2Pointer])
+
+            word1Pointer += 1
+            word2Pointer += 1
+
+        if word1Pointer < len(word1):
+            result.append(word1[word1Pointer:])
+        
+        if word2Pointer < len(word2):
+            result.append(word2[word2Pointer:])
+
+        return "".join(result)
+        
